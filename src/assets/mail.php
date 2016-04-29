6cd21d7235bfadf90b3e36b7c0c8573a665c1642
Inverter of Control <tumbleweedxl@outlook.com>
2016-04-29 09:56:08 +0000

    fixed mail handling

--- src/assets/mail.php
+++ src/assets/mail.php
@@ -2,9 +2,11 @@
 $to      = 'info@saschalaabs.com';
 $subject = 'Mail per Saschalaabs.com';
 
-$from = $_POST['from'];
+$postdata = file_get_contents("php://input");
+$request = json_decode($postdata);
+$from = $request->from;
+$message = $request->message;
 
-$message = $_POST['message'];
 $headers = 'From: '.$from . "\r\n" . 'Reply-To: ' . $from. "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
